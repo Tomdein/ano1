ae3a44cbee46d474b31d4d788ec2833aa8c5cd2a
Tomas Deingruber <Deingruber.Tomas@gmail.com>
2024-05-06 13:48:03 +0200

    Partialy reworks HoG

--- lib/hog.cpp
+++ lib/hog.cpp
@@ -7,83 +7,100 @@ namespace ano
 {
     // Compute HOG
     // If cells_in_block_count * cell_size != image size -> the histogram is averaged from less pixels
-    cv::Mat HoG(const cv::Mat &img, const int cells_in_block_count, const int cell_size, const int nbins)
+    cv::Mat HoG(const cv::Mat &img_gradients, const int cells_in_block_count, const int cell_size, const int nbins)
     {
-        int y_cell_count = static_cast<int>(std::ceil(img.size[0] / cell_size)); // How many cells in y dir
-        int x_cell_count = static_cast<int>(std::ceil(img.size[1] / cell_size)); // How many cells in x dir
+        int y_cell_count = static_cast<int>(std::ceil(img_gradients.size[0] / cell_size)); // How many cells in y dir
+        int x_cell_count = static_cast<int>(std::ceil(img_gradients.size[1] / cell_size)); // How many cells in x dir
 
-        int x_block_count = static_cast<int>(std::ceil(img.size[1] / (cells_in_block_count * cell_size))); // How many blocks in x dir
+        int y_block_count = static_cast<int>(std::ceil(img_gradients.size[0] / (cells_in_block_count * cell_size))); // How many blocks in x dir
+        int x_block_count = static_cast<int>(std::ceil(img_gradients.size[1] / (cells_in_block_count * cell_size))); // How many blocks in x dir
 
-        cv::Mat histogram(y_cell_count, x_cell_count, CV_32FC(nbins)); // The final histogram
+        cv::Mat img_histogram(y_cell_count, x_cell_count, CV_32FC(nbins)); // The final histogram
 
-        int *histogram_block_count = new int[x_block_count];                          // n ints for every orientation + 1 for pixel count ()
-        int *histogram_block_count_end = histogram_block_count + (x_block_count + 1); // Iterator end
-        int *histogram_block_count_counts_it = histogram_block_count;                 // Iterator
+        // 1. Add gradient to cell's histogram
 
-        // TODO: int *histogram_counts = new int[y_cell_count * x_cell_count * nbins];
-        // TODO: Single 2D array
-        int *histogram_counts = new int[x_cell_count * nbins];                     // n ints for every orientation + 1 for pixel count ()
-        int *histogram_counts_end = histogram_counts + (x_cell_count * nbins + 1); // Iterator end
-        int *histogram_counts_it = histogram_counts;                               // Iterator
+        // Counts histogram occurances
+        int *histogram_counts = new int[y_cell_count * x_cell_count * nbins];                     // n ints for every orientation + 1 for pixel count ()
+        int *histogram_counts_end = histogram_counts + (y_cell_count * x_cell_count * nbins + 1); // Iterator end
+        int *histogram_counts_it = histogram_counts;                                              // Iterator
 
-        for (int y = 0; y < y_cell_count; ++y)
+        // Zero the histogram counts
+        memset(histogram_counts, 0, y_cell_count * x_cell_count * nbins);
+
+        // Angle difference between bins
+        const float bin_delta = 360.0f / nbins;
+
+        // For every pixel in y direction
+        for (int y = 0; y < img_gradients.size[0]; y++)
         {
-            // Outside of image in y dir -> calculate histograms and skip
-            if (y >= img.size[0])
+            // Current cell in y dir
+            int y_cell = y / cell_size;
+
+            // For every pixel in x direction
+            for (int x = 0; x < img_gradients.size[1]; x++)
             {
-                continue;
-            }
+                // Current cell in x dir
+                int x_cell = x / cell_size;
 
-            // Just got outside previous cell -> calculate histograms and zero the histogram counts
-            if ((y + 1) % cell_size == 0)
-            {
-                // Normalize cells over single block
-                // For every block
-                for (int x = 0; x < x_block_count; ++x)
-                {
-                    histogram_counts_it = histogram_counts + x * nbins;
+                // Move pointer to current cell's histogram counts
+                histogram_counts_it = histogram_counts + (y_cell * x_cell_count + x_cell) * nbins;
 
-                    // Normalize cells over single block
-                    for (int i = 0; i < nbins; ++i)
-                    {
-                        // Divide by the number of pixels in the block
-                        histogram_counts_it[i] /= static_cast<float>(histogram_block_count[x]);
+                // Calculate bin
+                auto pixel = img_gradients.at<cv::Vec2f>(y, x);
+                int bin = static_cast<int>(std::floor(pixel[0] / bin_delta));
 
-                        // Increment pointer to the beginning of the next histogram data
-                        histogram_counts_it += nbins;
-                    }
-                }
+                assert(bin >= 0 && bin < nbins);
 
-                // Save the histograms
-
-                // Zero the histogram counts
-                histogram_counts_it = histogram_counts;
-                for (; histogram_counts_it != histogram_counts_end; histogram_counts_it++)
-                {
-                    *histogram_counts_it = 0;
-                }
-            }
-
-            // Calculate histograms for every pixel in cell
-
-            for (int x = 0; x < x_cell_count; ++x)
-            {
-                // Outside of image in x dir -> skip
-                if (x >= img.size[1])
-                {
-                    continue;
-                }
-
-                auto x_pixels_in_cell = cells_in_block_count * cell_size; // Number of pixels in one cell in x direction
-                for (int i = 0; i < x_pixels_in_cell; ++i)
-                {
-                }
-
-                // Sum histogram counts for every pixel
+                // Add +1 to bin histogram
+                *(histogram_counts_it + bin) += 1;
             }
         }
 
+        // 2. Sum all gradients in each block
+
+        // Counts number of pixels in cell
+        int *histogram_cell_pixels_count = new int[y_cell_count * x_cell_count];                                // n ints for every orientation + 1 for pixel count ()
+        int *histogram_cell_pixels_count_end = histogram_cell_pixels_count + (y_cell_count * x_cell_count + 1); // Iterator end
+        int *histogram_cell_pixels_count_counts_it = histogram_cell_pixels_count;                               // Iterator
+
+        // Zero the pixel counts for cell
+        memset(histogram_cell_pixels_count, 0, y_cell_count * x_cell_count);
+
+        // Counts histogram occurances
+        float *histogram_block_pixels_count = new float[y_block_count * x_block_count];                               // n floats for every orientation + 1 for pixel count ()
+        float *histogram_block_pixels_count_end = histogram_block_pixels_count + (y_block_count * x_block_count + 1); // Iterator end
+        float *histogram_block_pixels_count_it = histogram_block_pixels_count;                                        // Iterator
+
+        // Zero the histogram counts
+        memset(histogram_block_pixels_count, 0, y_block_count * x_block_count);
+
+        for (int y = 0; y < y_cell_count; y++)
+        {
+            // Current cell in y dir
+            int y_block = y / cells_in_block_count;
+
+            for (int x = 0; x < x_cell_count; x++)
+            {
+                int x_block = x / cells_in_block_count;
+            }
+        }
+
+        // 3. Normalize the cell's histogram by block's gradient sum
+
+        for (int y = 0; y < y_cell_count; y++)
+        {
+            // Current cell in y dir
+            int y_block = y / cells_in_block_count;
+
+            for (int x = 0; x < x_cell_count; x++)
+            {
+                int x_block = x / cells_in_block_count;
+            }
+        }
+
+        delete[] (histogram_block_pixels_count);
         delete[] (histogram_counts);
+        delete[] (histogram_cell_pixels_count);
 
         return;
     }
